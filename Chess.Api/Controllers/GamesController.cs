ef4f05431bf67e333702cfe8972a207e34738a48
aidan-marshall <aidanmarshall4@gmail.com>
2026-02-08 18:53:06 +0200

    Adding API and endpoints

---
+++ Chess.Api/Controllers/GamesController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Chess.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GamesController : ControllerBase
+{
+}
---
+++ Chess.Application/Dtos/MakeMoveDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Chess.Application.Dtos;
+
+public sealed record MakeMoveDto
+{
+    public required string FromSquare { get; init; }
+    public required string ToSquare { get; init; }
+    public string? PromotionPiece { get; init; }
+}
